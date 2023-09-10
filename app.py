f07aa1dd78afb9ae2a8cd63504f506c3d9cbb492
Leopold67 <jayminjhaveri10@gmail.com>
2023-09-10 04:40:17 -0400

    summary, email

--- app.py
+++ app.py
@@ -1,24 +1,35 @@
-import os
+import os, csv
+import base64
+
 from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
 import json
 
+from zipfile import ZipFile
+
+## MP4 TO MP3 ##
 from moviepy.editor import *
 
 from dotenv import load_dotenv
 
+## METAPHOR ##
+from metaphor_python import Metaphor
+
+## EMAIL SENDING ##
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import (Mail, Attachment, FileContent, FileName, FileType, Disposition)
+
 import openai
 
 UPLOAD_FOLDER = './uploads'
-ALLOWED_EXTENSIONS = {'mp4'}
+ALLOWED_EXTENSIONS = {'zip'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SECRET_KEY'] = os.getenv("FLASK_SECRET")
 
 load_dotenv()
-
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -43,8 +54,6 @@ def transcribe(filename):
 
 
 def save_transcript_file(content):
-
-    print("running")
 
     transcription = open("./build/transcription.txt", "w+")
     transcription.write(content)
@@ -117,15 +126,68 @@ def run_transcription(audio_file):
 
 
 def generate_summary(transcript):
+    print("Generating Summary")
+
     response = openai.ChatCompletion.create(
         model="gpt-4",
         messages=[
-            {"role": "user", "content": "You are a helpful assistant, your task is to write a summary with key points and the minutes of the meeting if it is a meeting based on the following transcript: {0}".format(
+            {"role": "user", "content": "You are a helpful assistant, your task is to write a summary with key points and the minutes of the meeting if it is a meeting based on the following transcript that is going to be emailed to the attendes and other stakeholders format it in html so that it can be emailed easily: {0}".format(
                 transcript)},
         ]
     )
 
     return response["choices"][0]["message"]["content"]
+
+def metaphor_ref(transcript):
+    print("Generating Readings")
+
+    metaphor = Metaphor(os.getenv("METAPHOR_API_KEY"))
+    search = "The most helpful resources about "
+    search += transcript
+    search += " is this:"
+    response = metaphor.search(search, num_results=5, use_autoprompt=False)
+
+    return(response)
+
+def passCSv(sheet):
+    # opening the CSV file
+    with open(sheet, mode ='r') as file:
+		
+        # reading the CSV file
+        csvFile = csv.DictReader(file)
+        list_of_contacts= list(csvFile)
+
+        # return list
+
+        return list_of_contacts
+
+
+def send_mail(summary, readings):
+    message = Mail(
+    from_email='jayminjhaveri10@gmail.com',
+    to_emails=passCSv(),
+    subject='Sending with Twilio SendGrid is Fun',
+    html_content='<h1>Hey!</h1></br><p>Here is what happened today:</p></br>{0}</br><p>Here are some readings you can refer to </p></br>{1}'.format(summary,readings))
+
+    with open('./uploads/resources.zip', 'rb') as f:    
+        data = f.read()
+        f.close()
+        encoded_file = base64.b64encode(data).decode()
+
+    attachedFile = Attachment(
+        FileContent(encoded_file),
+        FileName('resources.zip'),
+        FileType('application/zip'),
+        Disposition('attachment')
+    )
+
+    message.attachment = attachedFile
+
+
+    sg = SendGridAPIClient(os.getenv('SENDGRID_API_KEY'))
+    response = sg.send(message)
+
+    print(response.status_code)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -140,16 +202,30 @@ def upload_file():
             return 'No selected file'
 
         if file and allowed_file(file.filename):
+
             filename = "videofile.mp4"
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+
+            with ZipFile("./uploads/dispatch.zip", 'r') as zObject:
+                 zObject.extractall(path="./uploads")
+
+            os.remove("./uploads/dispatch.zip")
 
             converter(filename)
 
             transcript = run_transcription("audiofile.mp3")
             summary = generate_summary(transcript)
+            readings = metaphor_ref(transcript)
 
-            print(summary)
+            send_mail(summary, readings)
 
             return "Done!"
+            
 
     return "hello"
+
+@app.route('/test', methods=['GET', 'POST'])
+def test():
+    # passCSv("./uploads/people.csv")
+    send_mail()
+    return "ok"
